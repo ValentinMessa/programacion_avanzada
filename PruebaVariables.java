3d57bada6dd0396aa9cab6bef613f4ba2194cde8
ValentinMessa <canvamessa@gmail.com>
2025-05-21 22:08:48 -0300

    Prueba
    Valentin Messa

---
+++ PruebaVariables.java
@@ -0,0 +1,55 @@
+public class PruebaVariables {
+    
+public static void main(String[] args) {
+    //Valentin Messa
+    // 1. Declara una variable int llamada edad y asígnale el valor 17. Luego, imprímela por consola.
+    //int edad = 17;
+    //System.out.println("La edad es: "+edad);
+    // 2. Declara una variable double llamada precioProducto con el valor 45.99
+    //double precioProducto = 45.99;
+    // 3. Crea una variable boolean llamada tieneDescuento y asígnale el valor true.
+    //boolean tieneDescuento = true;
+    // 4. Declara una variable char que contenga la letra inicial de tu nombre.
+    //char letra = 'v';
+    // 5. Declara una variable int llamada puntos, asígnale 10, luego cambia su valor a 20 y vuelve a imprimirlo.
+    //int puntos = 10;
+    //System.out.println(puntos);
+    //puntos = 20;
+    //System.out.println(puntos);
+    // 6. Declara una variable double llamada notaFinal. Luego crea una condición que imprima "Aprobado" si la nota es mayor o igual a 6, o "Desaprobado" si es menor.
+    //double notafinal = 8;
+    //System.out.println(notafinal>=6?"Aprobado":"Desaprobado");
+    // 7. Declara una variable double llamada temperatura sin asignarle valor. Luego asígnale 36.6 e imprímela.
+    
+    //double Temperatura ;
+    //Temperatura = 36.6;
+
+    //System.out.println(Temperatura);
+    
+    // 8. Declara una variable int con un nombre inválido (por ejemplo, que empiece con un número). ¿Qué error te da el compilador?
+    //int 2ombre = 5;
+   //Syntax error on token "2"; delete this token    
+   
+   // 9. Declara una variable que almacene si el usuario es mayor de edad (usa boolean) y muestra un mensaje condicional.
+    //boolean mayorDeEdad = false;
+    //System.out.println( mayorDeEdad?"El usuario es mayor de edad":"El usuario es menor de edad");
+    // 10. Declara tres variables del mismo tipo int en una sola línea: x, y, z, y asígnales valores diferentes.
+    //int x = 9; ; int y = 7; ; int z = 3;
+    // 1. ¿Qué es una variable en Java y para qué se usa?
+    //Una variable en java es un pedacito de memoria para almacenar un dato que nosotros le asignemos 
+    // 2. Nombra y explica brevemente cuatro tipos de datos primitivos en Java.
+    //
+    // 3. ¿Cuál es la diferencia entre declarar una variable y asignarle un valor?
+    // La diferencia es que al declarar una variable elegis el tipo de variable que queres utilizar dependiendo de lo que quieras hacer, texo o numeros enteros y despues le asignamos el valor que quieramos o necesitemos.
+    // 4. ¿Qué significa que Java es un lenguaje "fuertemente tipado"?
+    // Pienso que es que no se pueden asignar valores a variables incorrectas, por ejemplo asignarle un numero entero a un string que es para cadena de texto.
+    // 6. ¿Por qué es importante usar nombres descriptivos en las variables?
+    // Es importante para que se entienda el programa o que lo demas lo entiendan.
+    //8. ¿Qué pasa si declaras una variable con una palabra reservada de Java?
+    // Te da error y no corre el programa.
+    
+	
+
+    
+ }
+}
